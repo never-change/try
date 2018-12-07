3f73b02684ef21a4b82ee0a677541ecdaa84d247
zj <1349265742@qq.com>
2018-12-07 19:22:53 +0800

    ...

--- pscloud_training/__manifest__.py
+++ pscloud_training/__manifest__.py
@@ -13,7 +13,7 @@
         #'views/res_partner_views.xml',
         #'views/training_lesson_views.xml',
         #'views/training_subject_views.xml',
-        #'views/training_views.xml',
+        'views/training_views.xml',
     ],
     'demo': [
         #'demo/pscloud_demo.xml',
